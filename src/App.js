3e4cdc992692a95061063092781806f9a4b92075
Luis1D <deanda_luis@outlook.com>
2021-10-25 05:13:40 -0700

    app converted to display whole month instead of week

--- src/App.js
+++ src/App.js
@@ -4,66 +4,70 @@ import React, { useState } from 'react';
 function App() {
   const [date, setTheDate] = useState();
   const [week, setWeek] = useState();
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
 
   const findWeek = (dateInput) => {
-    let firstHalf = new Date(dateInput);
-    let secondHalf = new Date(dateInput);
-    let dateObjHelper = new Date(dateInput);
-    let count = dateObjHelper.getDay();
-    let forward = [[firstHalf.toLocaleString('en-us', {
-      weekday: 'long' }), firstHalf.toLocaleString()]];
-    let backward = [];
-    while (count > 0) {
-      firstHalf.setDate(firstHalf.getDate() -1);
-      backward.unshift([firstHalf.toLocaleString('en-us',
-      { weekday: 'long' }), firstHalf.toLocaleString()]);
-      count--;
-    }
-    count = dateObjHelper.getDay();
-    while (count < 6) {
-      secondHalf.setDate(secondHalf.getDate() + 1);
-      forward.push([secondHalf.toLocaleString('en-us',
-      { weekday: 'long' }), secondHalf.toLocaleString()]);
+    let dateObj = new Date(dateInput);
+    let lastDay = new Date(dateObj.setFullYear(dateObj.getFullYear(), dateObj.getMonth() + 1, 0));
+    let count = 1;
+    let monthArr = [];
+    while (count <= lastDay.getDate()) {
+      let dayInFocus = new Date(dateObj.setFullYear(dateObj.getFullYear(), dateObj.getMonth(), count)); 
+      monthArr.push(dayInFocus)
       count++;
     }
-    const fullWeek = backward.concat(forward);
-    return fullWeek;
+    return monthArr;
   }
 
-  const handleChange = e => {
-    setTheDate(e.target.value);
-  }
-  const handleSubmit = e => {
-    e.preventDefault();
-    let week = findWeek(date);
-    setWeek(week);
-  }
+  // const handleChange = e => {
+  //   setTheDate(e.target.value);
+  // }
+  // const handleSubmit = e => {
+  //   e.preventDefault();
+  //   let week = findWeek(date);
+  //   setWeek(week);
+  // }
 
   const defaultWeek = () => {
     let today = new Date();
     setTheDate(today);
     let currentWeek = findWeek(today)
     setWeek(currentWeek);
-    console.log("yo",date)
+  }
+
+  const nextMonth = () => {
+    
+  }
+
+  const prevMonth = () => {
+
   }
 
   return (
     <div className="App">
-      <h1>Week Lookup</h1>
-      <form onSubmit={ handleSubmit }>
+      {/* <form onSubmit={ handleSubmit }>
         <input type="text" placeholder="XX/XX/XXXX" onChange={handleChange} className="text-input"/>
         <input type="submit" className="input-btn"/>
       </form>
-      <div className="card-container">
-      {
-        week ? week.map(day => {
-          return <div key={day[1]} className="week-card">
-            
-            <span>{day[0]}</span>
-            <span>{day[1].split(",")[0]}</span>
-          </div>
-        }) : defaultWeek()
-      }
+      <hr /> */}
+      <div className="card-wrapper">
+        <div className="month-picker">
+          <button className="month-picker-btn" onClick={ prevMonth }>{"<"}</button>
+          { date ? <h2 className="month">{ months[date.getMonth()] + " " + date.getFullYear() }</h2> : null }
+          <button className="month-picker-btn" onClick={ nextMonth }>{">"}</button>
+        </div>
+        <div className="card-container">
+        {
+          week ? week.map(day => {
+            return <div key={day} className={ day.getDate() === date.getDate() ? "spot-light week-card" : "week-card" }>
+              <span className="day-of-week">{ days[day.getDay()] }</span>
+              <span className="date">{ day.getDate() }</span>
+            </div>
+          }) : defaultWeek()
+        }
+        </div>
       </div>
     </div>
   );
