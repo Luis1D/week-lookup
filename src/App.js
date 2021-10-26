5248d94fbd24ac476a6e75352da82e5d52ad9c1b
Luis1D <deanda_luis@outlook.com>
2021-10-25 22:04:42 -0700

    added functionality to add a note on a selected date

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import React, { useState } from 'react';
 function App() {
   const [date, setTheDate] = useState({ value: new Date(), query: false, queryData: "" });
   const [week, setWeek] = useState();
-  const [selectedDate, setSelectedDate] = useState();
+  const [noteProcess, setNoteProcess] = useState({
+    list: {
+      title: "",
+      description: ""
+    },
+    noteInProgress: false
+  });
+  const [notesList, setNotesList] = useState([]);
   const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   const days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
 
@@ -27,8 +34,8 @@ function App() {
   const handleSubmit = e => {
     e.preventDefault();
     setTheDate({ value: new Date(date.queryData), query: true });
-    let week = findWeek(date.value);
-    setWeek(week);
+    let weekToUse = findWeek(date.value);
+    setWeek(weekToUse);
   }
 
   const defaultWeek = () => {
@@ -39,14 +46,14 @@ function App() {
   const nextMonth = () => {
     const month = new Date(date.value.setFullYear(date.value.getFullYear(), date.value.getMonth() + 1, 1))
     setTheDate(month);
-    let week = findWeek(month);
-    setWeek(week);
+    let weekToUse = findWeek(month);
+    setWeek(weekToUse);
   }
   const prevMonth = () => {
     const month = new Date(date.value.setFullYear(date.value.getFullYear(), date.value.getMonth() - 1, 1))
     setTheDate(month);
-    let week = findWeek(month);
-    setWeek(week);
+    let weekToUse = findWeek(month);
+    setWeek(weekToUse);
   }
 
   const dateSelectionStyles = day => {
@@ -56,8 +63,37 @@ function App() {
     return "week-card";
   }
   
+  const addNote = (e) => {
+    let time = new Date();
+    let timeCreated = time.toLocaleTimeString("en-US");
+  
+    const value = e.target.value;
+    setNoteProcess({
+      list: {
+        ...noteProcess.list,
+        [e.target.name]: value,
+        date: date,
+        time: timeCreated
+      },
+      noteInProgress: true
+    })
+  }
+  const submitNote = e => {
+    e.preventDefault()
+    setNotesList([...notesList, noteProcess.list]);
+    setNoteProcess({ 
+      list: {
+        title: "",
+        description: ""
+      },
+      noteInProgress: !noteProcess.noteInProgress  
+    })
+  }
   return (
     <div className="App">
+      {
+        notesList.length > 0 ? console.log(notesList) : null
+      }
       <form onSubmit={ handleSubmit }>
         <input type="text" placeholder="Lookup a date.." onChange={handleChange} className="text-input"/>
         <input type="submit" className="input-btn"/>
@@ -84,12 +120,30 @@ function App() {
         </div>
       </div>
       <div className="date-details">
+        <div className="wrapper">
+          {
+            date.value ? <div>
+              <h3>{ days[date.value.getDay()] + " " + months[date.value.getMonth()] + " " + date.value.getDate() + " " + date.value.getFullYear() }</h3> 
+            </div> : null
+          }
+          {
+            noteProcess.noteInProgress ? <span className="submit-note btn" onClick={ submitNote }>Submit</span>
+            : <span className="btn" onClick={ () => setNoteProcess({list: noteProcess.list, noteInProgress: true }) }>Add Note</span>
+          }
+        </div>
         {
-          date.value ? <div>
-            <h3>{ days[date.value.getDay()] + " " + months[date.value.getMonth()] + " " + date.value.getDate() + " " + date.value.getFullYear() }</h3> 
-          </div> : null
+          noteProcess.noteInProgress ? <form className="note-form" onSubmit={ submitNote }>
+            <input name="title" value={ noteProcess.list.title } type="text" placeholder="Title" onChange={ addNote }/>
+            <textarea name="description" value={ noteProcess.list.description } type="text" placeholder="Description" onChange={ addNote }/>
+          </form>
+          : null
         }
       </div>
+      {
+        date.value ? <div className="events">
+          <h3>{`All notes for ${months[date.value.getMonth()]}`}</h3>
+      </div> : null
+      }
     </div>
   );
 }
