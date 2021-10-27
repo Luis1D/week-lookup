5070bd3e19e62b2810f54961d2798ff5b7d421b7
Luis1D <deanda_luis@outlook.com>
2021-10-26 19:39:02 -0700

    add note functionality added

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
     noteInProgress: false
   });
   const [notesList, setNotesList] = useState([]);
+
   const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   const days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
 
@@ -44,14 +45,16 @@ function App() {
   }
 
   const nextMonth = () => {
-    const month = new Date(date.value.setFullYear(date.value.getFullYear(), date.value.getMonth() + 1, 1))
-    setTheDate(month);
+    let dateRef = new Date(date.value);
+    const month = new Date(dateRef.setFullYear(dateRef.getFullYear(), dateRef.getMonth() + 1, 1))
+    setTheDate({ value: month });
     let weekToUse = findWeek(month);
     setWeek(weekToUse);
   }
   const prevMonth = () => {
-    const month = new Date(date.value.setFullYear(date.value.getFullYear(), date.value.getMonth() - 1, 1))
-    setTheDate(month);
+    let dateRef = new Date(date.value);
+    const month = new Date(dateRef.setFullYear(dateRef.getFullYear(), dateRef.getMonth() - 1, 1))
+    setTheDate({ value: month });
     let weekToUse = findWeek(month);
     setWeek(weekToUse);
   }
@@ -73,7 +76,8 @@ function App() {
         ...noteProcess.list,
         [e.target.name]: value,
         date: date,
-        time: timeCreated
+        time: timeCreated,
+        id: Date.now()
       },
       noteInProgress: true
     })
@@ -89,20 +93,27 @@ function App() {
       noteInProgress: !noteProcess.noteInProgress  
     })
   }
+  
+  const deleteNote = id => {
+    let filteredList = notesList.filter(note => {
+      return note.id !== id;
+    })
+    setNotesList(filteredList);
+  }
+
   return (
     <div className="App">
-      {
-        notesList.length > 0 ? console.log(notesList) : null
-      }
       <form onSubmit={ handleSubmit }>
         <input type="text" placeholder="Lookup a date.." onChange={handleChange} className="text-input"/>
         <input type="submit" className="input-btn"/>
       </form>
       <div className="card-wrapper">
         <div className="month-picker">
+
           <button className="month-picker-btn" onClick={ prevMonth }>{"<"}</button>
           { date.value ? <h2 className="month">{ months[date.value.getMonth()] + " " + date.value.getFullYear() }</h2> : null }
           <button className="month-picker-btn" onClick={ nextMonth }>{">"}</button>
+
         </div>
         <div className="card-container">
         {
@@ -132,7 +143,7 @@ function App() {
           }
         </div>
         {
-          noteProcess.noteInProgress ? <form className="note-form" onSubmit={ submitNote }>
+          noteProcess.noteInProgress ? <form className="note-form">
             <input name="title" value={ noteProcess.list.title } type="text" placeholder="Title" onChange={ addNote }/>
             <textarea name="description" value={ noteProcess.list.description } type="text" placeholder="Description" onChange={ addNote }/>
           </form>
@@ -142,6 +153,25 @@ function App() {
       {
         date.value ? <div className="events">
           <h3>{`All notes for ${months[date.value.getMonth()]}`}</h3>
+          {
+            notesList.length > 0 ?
+            notesList.map(note => {
+              if (note.date.value.getMonth() === date.value.getMonth()) {
+                return <div key={ note.id } className="notes-details-container">
+                  <div className="title-desc">
+                    <span className="title">{ note.title }</span>
+                    <span className="date">{ note.date.value.toLocaleString().split(",")[0] }</span>
+                    <span className="description">{ note.description }</span>
+                  </div>
+                  <div className="date-delete-container">
+                    <button className="edit-btn">Edit</button>
+                    <button className="delete-btn" onClick={ () => deleteNote(note.id) }>Delete</button>
+                  </div>
+                </div>
+              }
+              return <div></div>;
+            }) : null
+          }
       </div> : null
       }
     </div>
